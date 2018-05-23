33cdea2155344c23e5b5e67c2a17fff6ca6adf78
Craig <craig.allwardt@pnnl.gov>
2018-05-23 10:22:50 -0700

    Updated gridappsd module to walk a person through most of the api.

--- gridappsd/__main__.py
+++ gridappsd/__main__.py
@@ -20,21 +20,38 @@ if __name__ == '__main__':
             print("headers: {}\nmessage: {}".format(headers, message))
 
     def on_message(headers, message):
-        print("function headers: {}\nmessage: {}".format(headers, message))
+        print("Received: headers: {}\nmessage: {}".format(headers, message))
 
+    print("Creating GridAPPSD object")
     gapps = GridAPPSD(stomp_address="127.0.0.1",
                       stomp_port=61613,
                       username='system',
                       password='manager')
 
+    print("Subscribing to /topic/foo")
     gapps.subscribe('/topic/foo', on_message)
-    sleep(1)
+    input("Press enter to send dict(bim='bash') to topic /topic/foo")
+    print("Sending data")
     gapps.send('/topic/foo', json.dumps(dict(bim='bash')))
+    sleep(1)
 
-
+    input("Press enter to receive platform status")
     resp = gapps.get_platform_status()
-    _log.debug("Got response of: {}".format(resp))
+    pprint(resp)
 
+    input("Press enter to query model info")
+    resp = gapps.query_model_info()
+    pprint(resp)
+
+    input("Press enter to query model names")
+    resp = gapps.query_model_names()
+    pprint(resp)
+
+    input("Press enter to query object types")
+    resp = gapps.query_object_types()
+    pprint(resp)
+
+    input("Press enter to run ad-hoc query")
     # list all the connectivity nodes by feeder - CIMImporter when building GldNodes
     sparql = """
 PREFIX r:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -49,21 +66,10 @@ SELECT ?feeder ?name WHERE {
 }
 ORDER by ?feeder ?name
     """
-    print("RESP 2" + "*" * 20)
-    resp2 = gapps.query_data(sparql)
-    pprint(resp2)
+    print("QUERY")
+    print(sparql)
+    print()
+    resp = gapps.query_data(sparql)
+    pprint(resp)
 
-    print("RESP 3" + "*" * 20)
-    resp3 = gapps.query_model_names()
-    pprint(resp3)
-
-    print("RESP 4" + "*" * 20)
-    resp4 = gapps.query_object_types()
-    pprint(resp4)
-
-    count = 0
-    while count < 30:
-        sleep(1)
-        count += 1
-
-    print("complete")
+    input("Press enter to exit")
