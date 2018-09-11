9b248822c8c2f75cae8244c8da7986d8f6033018
C. Allwardt <craig.allwardt@pnnl.gov>
2018-09-11 16:24:26 -0400

    Updated to use utils for username password and address.

--- register_app.py
+++ register_app.py
@@ -3,41 +3,52 @@ import logging
 import os
 import sys
 import time
-import random
-
 import stomp
+from gridappsd import ApplicationController, GridAPPSD, utils
 
-from gridappsd import ApplicationController, GridAPPSD
-from gridappsd.topics import (
-    REQUEST_REGISTER_APP,
-    BASE_APPLICATION_HEARTBEAT,
-    REQUEST_APP_START
-)
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 _log = logging.getLogger(__name__)
 
-if not os.path.exists("/appconfig"):
+problems = utils.validate_gridappsd_uri()
+
+if problems:
+    for p in problems:
+        _log.error(p)
+    sys.exit(1)
+
+gridapspd_address = utils.get_gridappsd_address()
+gridapspd_user = utils.get_gridappsd_user()
+gridappsd_pass = utils.get_gridappsd_pass()
+
+if not os.path.isfile("/appconfig"):
     _log.error("Invalid /appconfig reference...map the /appconfig to your container")
     sys.exit(1)
 
-config = open("/appconfig").read()
+config = json.loads(open("/appconfig").read())
 
 while True:
 
     try:
-        gap = GridAPPSD()
+        gap = GridAPPSD(username=gridapspd_user, password=gridappsd_pass,
+                        address=utils.get_gridappsd_address())
     except stomp.exception.ConnectFailedException:
         _log.debug("Retry in 10 seconds")
         time.sleep(10)
+    # except TypeError:
+    #     _log.debug("Retry in 5 seconds")
+    #     time.sleep(5)
     else:
         # app_config_minimal = {
         #     'id': 'an-app-id',
         #     'execution_path': '/home/osboxes/git/gridappsd-python/testapp.sh'
         # }
-        appreg = ApplicationController(config, gridappsd=gap)
-        appreg.register_app()
-        break
+        try:
+            appreg = ApplicationController(config, gridappsd=gap)
+            appreg.register_app()
+            break
+        except ValueError:
+            sys.exit(1)
 
 while True:
     time.sleep(2)
