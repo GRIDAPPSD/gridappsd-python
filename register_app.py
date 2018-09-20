baec429f69667cc869282542a2acaeb4f1c248bf
Craig <craig.allwardt@pnnl.gov>
2018-09-20 12:34:15 -0400

    Handle reconnection.

--- register_app.py
+++ register_app.py
@@ -7,7 +7,9 @@ import stomp
 from gridappsd import ApplicationController, GridAPPSD, utils
 
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
+                    format="%(asctime)s;%(levelname)s;%(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
 _log = logging.getLogger(__name__)
 
 problems = utils.validate_gridappsd_uri()
@@ -26,31 +28,39 @@ if not os.path.isfile("/appconfig"):
     sys.exit(1)
 
 config = json.loads(open("/appconfig").read())
-
+appreg = None
 while True:
 
     try:
         gap = GridAPPSD(username=gridapspd_user, password=gridappsd_pass,
                         address=utils.get_gridappsd_address())
-    except stomp.exception.ConnectFailedException:
+
+    except ConnectionRefusedError:  # Python 3 specific error code
         _log.debug("Retry in 10 seconds")
+        appreg = None
+        time.sleep(10)
+    except stomp.exception.ConnectFailedException:
+        _log.debug("Connect failed Retry in 10 seconds")
+        appreg = None
         time.sleep(10)
     # except TypeError:
     #     _log.debug("Retry in 5 seconds")
     #     time.sleep(5)
     else:
-        # app_config_minimal = {
-        #     'id': 'an-app-id',
-        #     'execution_path': '/home/osboxes/git/gridappsd-python/testapp.sh'
-        # }
-        try:
-            appreg = ApplicationController(config, gridappsd=gap)
-            appreg.register_app()
-            break
-        except ValueError:
-            sys.exit(1)
+        if appreg is None:
+            def end_app():
+                sys.exit(0)
 
-while True:
-    time.sleep(2)
+            # app_config_minimal = {
+            #     'id': 'an-app-id',
+            #     'execution_path': '/home/osboxes/git/gridappsd-python/testapp.sh'
+            # }
+            try:
+                appreg = ApplicationController(config, gridappsd=gap)
+                appreg.register_app(end_app)
+            except ValueError:
+                sys.exit(1)
+
+        time.sleep(2)
 
 
