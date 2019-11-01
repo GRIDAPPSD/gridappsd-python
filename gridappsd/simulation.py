6de6c4f757367d9556bb336f9a1fe6bba8531aa0
Brandon Thayer <brandon.thayer@pnnl.gov>
2019-11-01 14:44:03 -0500

    Revert "WeakSets can't handle tuples so disabling the filtering ability for now."
    This reverts commit 013b3f6f48673590b131f10e83b51c047e7a1279.

--- gridappsd/simulation.py
+++ gridappsd/simulation.py
@@ -108,10 +108,7 @@ class Simulation(object):
         :param device_filter: Future filter of measurements
         :return:
         """
-        # self.__filterable_measurement_callback_set.add((callback, device_filter))
-        if device_filter:
-            raise NotImplemented("device filter not set at this time due to time constraints.")
-        self.__filterable_measurement_callback_set.add(callback)
+        self.__filterable_measurement_callback_set.add((callback, device_filter))
 
     def add_onstart_callback(self, callback):
         """ registers a start callback that is called when the simulation is started
@@ -189,4 +186,4 @@ class Simulation(object):
         timestamp = message['message']['timestamp']
         measurements = message['message']['measurements']
         for p in self.__filterable_measurement_callback_set:
-            p(self, timestamp, measurements)
+            p[0](self, timestamp, measurements)
