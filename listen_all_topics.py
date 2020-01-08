a0f82f3fe90270b36178f4cd5655f50ac01ed857
C. Allwardt <craig.allwardt@pnnl.gov>
2020-01-08 14:25:44 -0500

    Added error logging for failures

---
+++ listen_all_topics.py
@@ -0,0 +1,17 @@
+from time import sleep
+from gridappsd import GridAPPSD
+
+g = GridAPPSD()
+
+
+def cb(header, msg):
+    print(f"header: {header} message: {msg}")
+
+g.subscribe("/topic/data", cb)
+
+houses = g.get_houses()
+hs = houses.get_houses_for_feeder('_503D6E20-F499-4CC7-8051-971E23D0BF79')
+print(hs)
+
+while True:
+    sleep(0.1)
