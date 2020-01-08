48a903f370bb57ad13489058124619db23a51ab5
C. Allwardt <craig.allwardt@pnnl.gov>
2020-01-08 15:47:05 -0500

    Bump version of gridappsd-python

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@
 from os import path
 from setuptools import setup, find_packages
 
-__version__ = '2019.10.3-dev'
+__version__ = '2020.01.0-dev'
 
 # The directory containing this file
 HERE = path.dirname(__file__)
