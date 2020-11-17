67826f11b16e40a6bc7a4b18776efaa22400ac76
Craig <3979063+craig8@users.noreply.github.com>
2020-11-17 10:23:42 -0800

    Update setup.py
    Bump version to 2020.11.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@
 from os import path
 from setuptools import setup, find_packages
 
-__version__ = '2020.09.1'
+__version__ = '2020.11.0'
 
 # The directory containing this file
 HERE = path.dirname(__file__)
