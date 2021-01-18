d027fc8434cf627c181d6e0d711369a3f516862d
Craig <3979063+craig8@users.noreply.github.com>
2021-01-18 13:01:31 -0800

    Update conftest.py
    Fixed goss_client fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import os
 
 import pytest
 
-from gridappsd import GridAPPSD
+from gridappsd import GridAPPSD, GOSS
 from gridappsd.docker_handler import run_dependency_containers, run_gridappsd_container, Containers
 
 STOP_CONTAINER_AFTER_TEST = os.environ.get('GRIDAPPSD_STOP_CONTAINERS_AFTER_TESTS', True)
@@ -27,3 +27,14 @@ def gridappsd_client(docker_dependencies):
         yield gappsd
 
         gappsd.disconnect()
+
+@pytest.fixture
+def goss_client(docker_dependencies):
+    with run_gridappsd_container(stop_after=STOP_CONTAINER_AFTER_TEST):
+        goss = GOSS()
+        goss.connect()
+        assert goss.connected
+
+        yield goss
+
+        goss.disconnect()
