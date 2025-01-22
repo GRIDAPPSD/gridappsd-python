25c92d62998c76260a26bcc13332602127dff962
poorva1209 <poorva1209@gmail.com>
2025-01-21 23:06:41 -0800

    updated after testing

--- gridappsd-field-bus-lib/gridappsd/field_interface/field_proxy_forwarder.py
+++ gridappsd-field-bus-lib/gridappsd/field_interface/field_proxy_forwarder.py
@@ -1,63 +1,26 @@
 import stomp
 import json
+import time
 from typing import Callable, Dict
 from gridappsd import GridAPPSD
 from gridappsd import topics
 
-class FieldProxyForwarder():
-    """
-    FieldProxyForwarder acts as a bridge between field bus and OT bus 
-    when direct connection is not possible. 
-    """
-   
-    def __init__(self, connection_url: str, username: str, password: str):
+REQUEST_FIELD = ".".join((topics.PROCESS_PREFIX, "request.field"))
 
-        #Connect to proxy
-        self.broker_url = connection_url
-        self.username = username
-        self.password = password
-        self.proxy_connection = stomp.Connection([(self.broker_url.split(":")[0], int(self.broker_url.split(":")[1]))])
-        self.proxy_connection.connect(self.username, self.password, wait=True)
-        
-        
-        #Connect to OT
-        self.ot_connection = GridAPPSD()
+class FieldListener():
 
-        #Subscribe to messages from field
-        self.proxy_connection.set_listener('', self.on_message_from_field)
-        self.proxy_connection.subscribe(destination=topics.BASE_FIELD_TOPIC, id=1, ack="auto")
-        
-        #Subscribe to messages on OT bus
-        self.ot_connection.subscribe(topics.BASE_FIELD_TOPIC, self.on_message_from_ot)
-
-    def on_message_from_ot(self, headers, message):
-        "Receives messages coming from OT bus (GridAPPS-D) and forwards to Proxy bus"
-        try:
-            print(f"Received message from OT: {message}")
-
-            if headers["destination"] == topics.field_input_topic():
-                self.proxy_connection.send(topics.field_input_topic, message)
-        
-            elif "goss.gridappsd.field.simulation.output." in headers["destination"]:
-                print("Simulation output received at OT. Ignoring.")
-
-            else:
-                print(f"Unrecognized message received by OT: {message}")
-
-        except Exception as e:
-            print(f"Error processing message: {e}")
-
-
-
-    def on_message_from_field(self, headers, message):
+    def on_message(self, headers, message):
         "Receives messages coming from Proxy bus (e.g. ARTEMIS) and forwards to OT bus"
         try:
             print(f"Received message at Proxy: {message}")
 
             if headers["destination"] == topics.field_output_topic():
-                self.ot_connection.send(topics.field_output_topic, message)
-        
-            elif "context_manager" in headers["destination"]:
+                self.ot_connection.send(topics.field_output_topic(), message)
+            
+            elif headers["destination"] == topics.field_input_topic():
+                pass
+
+            elif headers["destination"] == REQUEST_FIELD:
                 request_data = json.loads(message)
                 request_type = request_data.get("request_type")
                 if request_type == "get_context":
@@ -70,10 +33,59 @@ class FieldProxyForwarder():
         except Exception as e:
             print(f"Error processing message: {e}")
 
-if __name__ == "__main__":
+class FieldProxyForwarder():
+    """
+    FieldProxyForwarder acts as a bridge between field bus and OT bus
+    when direct connection is not possible.
+    """
 
-    proxy_connection_url = "localhost:61613"
-    proxy_username = "admin"
-    proxy_password = "admin"
+    def __init__(self, connection_url: str, username: str, password: str):
+
+        #Connect to proxy
+        self.broker_url = connection_url
+        self.username = username
+        self.password = password
+        self.proxy_connection = stomp.Connection([(self.broker_url.split(":")[0], int(self.broker_url.split(":")[1]))],keepalive=True)
+        self.proxy_connection.set_listener('', FieldListener())
+        self.proxy_connection.connect(self.username, self.password, wait=True)
+        print('Connected to Proxy')
+
+        #Connect to OT
+        self.ot_connection = GridAPPSD()
+
+        #Subscribe to messages from field
+        self.proxy_connection.subscribe(destination=topics.BASE_FIELD_TOPIC+'.*', id=1, ack="auto")
+        
+        #Subscribe to messages on OT bus
+        self.ot_connection.subscribe(topics.field_input_topic(), self.on_message_from_ot)
+
+    def on_message_from_ot(self, headers, message):
+        "Receives messages coming from OT bus (GridAPPS-D) and forwards to Proxy bus"
+        try:
+            print(f"Received message from OT: {message}")
+
+            if headers["destination"] == topics.field_input_topic():
+                self.proxy_connection.send(topics.field_input_topic(), message)
+
+            else:
+                print(f"Unrecognized message received by OT: {message}")
+
+        except Exception as e:
+            print(f"Error processing message: {e}")
+       
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(prog="TestForwarder") 
+    parser.add_argument("username")
+    parser.add_argument("passwd")
+    parser.add_argument("connection_url")
+    opts = parser.parse_args()
+    proxy_connection_url = opts.connection_url
+    proxy_username = opts.username
+    proxy_password = opts.passwd
 
     proxy_forwarder = FieldProxyForwarder(proxy_connection_url, proxy_username, proxy_password)
+
+    while True:
+        time.sleep(0.1)
