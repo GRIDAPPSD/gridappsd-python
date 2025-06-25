db3b9628486d5252f486d2d93938fed22c48fc78
Craig <3979063+craig8@users.noreply.github.com>
2025-06-25 15:41:45 -0700

    Update simulation.py
    Remove trailing comma

--- gridappsd-python-lib/gridappsd/simulation.py
+++ gridappsd-python-lib/gridappsd/simulation.py
@@ -1,8 +1,5 @@
-<<<<<<< bugfix/152
 from dataclasses import dataclass, field
-=======
-from dataclasses import dataclass, field,
->>>>>>> develop
+
 import time
 import logging
 from typing import Dict, List, Union
